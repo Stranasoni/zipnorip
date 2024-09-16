e38d0941928b60c15953d14ec3e2c743ab020b7b
Soni <Dsn2004@mail.ru>
2024-09-16 23:19:26 +0300

    Реализована приоритетная очередь, по которой и будет строиться дерево Хаффмана. Один пункт выполнен. Далее нужно написать класс для построения дерева удачи

--- zipnorip2/main.cpp
+++ zipnorip2/main.cpp
@@ -1,4 +1,7 @@
 #include <unordered_map>
+#include "priority_queue.h"
+#include "priority_queue.cpp"
+#include "iostream"
 
 int main()
 {
@@ -6,4 +9,21 @@ int main()
 	//подсчет частоты() сохранить в unordered_map
 	//построение дерева хаффмана(нужно создать класс дерево которое будет
 		//иметь метод обхода в глубину(рекурсия), сложный метод который будет как https://cloudstreamgate.com/response/show_text/23b97289-38ca-49d9-b91f-a707c2bb6e61
+	//its work!!!!!
+	PriorityQueue<int> pq([](int* a, int* b) {return (*a) < (*b); });
+	std::vector<int>* v = new std::vector<int>{3,3,1,4,5,1,2,5};
+	std::vector<int> v2 = { 3, 1, 4, 5, 1, 2, 5 }; 
+	for (int i = 0; i < 6; ++i)
+	{
+		pq.push(&(*v)[i]);
+		pq.push(&v2[i]);
+	}
+	v = &v2;
+	v2.clear();
+	v2 = { 0,0,0,0,0,0 };
+	for (int i = 0; i < 12; ++i)
+	{
+		std::cout<< (*pq.pop())<<"\n";
+	}
+	
 }
--- zipnorip2/priority_queue.cpp
+++ zipnorip2/priority_queue.cpp
@@ -1,1 +1,87 @@
 #include "Priority_queue.h"
+
+template <typename T>
+PriorityQueue<T>::PriorityQueue(bool(*comparator)(T*, T*)) :comp(comparator) {};
+
+template <typename T>
+PriorityQueue<T>::PriorityQueue()
+{
+	comp = [](T* a, T* b) { return (*a) < (*b); };
+}
+
+template <typename T>
+T* PriorityQueue<T>::swap(size_t a, size_t b)
+{
+	T* temp = binary_min_heap[a];
+	binary_min_heap[a] = binary_min_heap[b];
+	binary_min_heap[b] = temp;
+	return (temp);
+
+}
+
+template <typename T>
+void PriorityQueue<T>::heapify_up(size_t index)
+{
+	if (index == 0) return;
+	size_t parent;
+	index % 2 == 0 ? parent = (index - 2) / 2 : parent = (index - 1) / 2;
+	//сын больше равен родителю тогда ничего, так как minheap
+	if (!comp(binary_min_heap[index], binary_min_heap[parent])) return;
+	swap(index, parent);
+	heapify_up(parent);
+
+}
+
+template <typename T>
+size_t PriorityQueue<T>::min(size_t l, size_t r)
+{
+	if (comp(binary_min_heap[r], binary_min_heap[l])) return r;
+	return l;
+}
+
+template <typename T>
+void PriorityQueue<T>::heapify_down(size_t index)
+{
+	size_t l = 2 * index + 1;
+	size_t r = 2 * index + 2;
+	//если узел - лист
+	if (!(l < binary_min_heap.size()) && !(r < binary_min_heap.size())) return;
+	//если родитель имеет только одного сына(правого брата не будет без левого)
+	if (!(r < binary_min_heap.size())) {
+		if (!(comp(binary_min_heap[l], binary_min_heap[index]))) return;
+		swap(index, l);
+		return;
+	}
+	//если родитель меньше обоих сыновей
+	if (!(comp(binary_min_heap[l], binary_min_heap[index]) || comp(binary_min_heap[r], binary_min_heap[index])))
+		return;
+
+	size_t next = min(l, r);
+	swap(index, next);//mmm
+	heapify_down(next);
+}
+
+template <typename T>
+T PriorityQueue<T>::top() const
+{
+	return *binary_min_heap[0];
+}
+
+template <typename T>
+void PriorityQueue<T>::push(T* item)
+{
+	binary_min_heap.push_back(item);
+	heapify_up(binary_min_heap.size() - 1);
+
+}
+
+//управление указателем передается области вызова (нельзя не присвоить =>обернуть в умные указатели)
+template <typename T>
+T* PriorityQueue<T>::pop()
+{
+	T* root = swap(0, binary_min_heap.size() - 1);
+	binary_min_heap.resize(binary_min_heap.size() - 1);
+	heapify_down(0);
+	return root;// 
+
+}
